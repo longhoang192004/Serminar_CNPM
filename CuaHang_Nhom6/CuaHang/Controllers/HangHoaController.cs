46528917509f322d554e30ec57c831fe1f714886
longhoang192004 <132119956+longhoang192004@users.noreply.github.com>
2024-11-27 18:35:53 +0700

    Add files via upload

---
+++ CuaHang_Nhom6/CuaHang/Controllers/HangHoaController.cs
@@ -0,0 +1,135 @@
+﻿using CuaHang.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CuaHang.Controllers
+{
+    public class HangHoaController : Controller
+    {
+        // GET: HangHoa
+        CuaHangEntities1 db = new CuaHangEntities1();
+        public ActionResult Index(string txtSearch = "", int? loaiHangId = null)
+        {
+            ViewBag.LoaiHangList = new SelectList(db.LoaiHangs.ToList(), "MaLH", "TenLH");
+
+            var hangHoas = db.HangHoas.AsQueryable();
+
+            if (!string.IsNullOrEmpty(txtSearch))
+            {
+                hangHoas = hangHoas.Where(hh => hh.TenHH.Contains(txtSearch));
+            }
+
+            if (loaiHangId.HasValue)
+            {
+                hangHoas = hangHoas.Where(hh => hh.MaLH == loaiHangId);
+            }
+
+            return View(hangHoas.ToList());
+        }
+
+
+        public ActionResult ThanhToan()
+        {
+            Session["GioHang"] = null;
+            TempData["Message"] = "Thanh toán thành công";
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult Mua(int id)
+        {
+            return View(db.HangHoas.Find(id));
+        }
+
+        public ActionResult GioHang(int id)
+        {
+            List<HangHoa> gioHang = Session["GioHang"] as List<HangHoa> ?? new List<HangHoa>();
+            return View(db.HangHoas.Find(id));
+        }
+
+
+        public ActionResult XoaKhoiGioHang(int id)
+        {
+            List<HangHoa> gioHang = Session["GioHang"] as List<HangHoa> ?? new List<HangHoa>();
+            var itemToRemove = gioHang.FirstOrDefault(h => h.MaHH == id);
+            if (itemToRemove != null)
+            {
+                gioHang.Remove(itemToRemove);
+            }
+            Session["GioHang"] = gioHang;
+            return RedirectToAction("GioHang");
+        }
+
+
+
+
+
+
+        public ActionResult Create()
+        {
+            ViewBag.MaLH = new SelectList(db.LoaiHangs.ToList(), "MaLH", "TenLH");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(HangHoa hangHoa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.HangHoas.Add(hangHoa);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MaLH = new SelectList(db.LoaiHangs.ToList(), "MaLH", "TenLH", hangHoa.MaLH);
+            return View(hangHoa);
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View(db.HangHoas.Find(id));
+        }
+
+        public ActionResult Edit(int id)
+        {
+            ViewBag.MaLH = new SelectList(db.LoaiHangs.ToList(), "MaLH", "TenLH");
+            return View(db.HangHoas.Find(id));
+        }
+        [HttpPost]
+        public ActionResult Edit(HangHoa hangHoa)
+        {
+            if (ModelState.IsValid)
+            {
+                HangHoa hh = db.HangHoas.Find(hangHoa.MaHH);
+                hh.TenHH = hangHoa.TenHH;
+                hh.MaLH = hangHoa.MaLH;
+                hh.Gia = hangHoa.Gia;
+                hh.SLTon = hangHoa.SLTon;
+                hh.DonVi = hangHoa.DonVi;
+
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MaLH = new SelectList(db.LoaiHangs.ToList(), "MaLH", "TenLH", hangHoa.MaLH);
+            return View(hangHoa);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View(db.HangHoas.Find(id));
+        }
+        public ActionResult ConfirmDelete(int id)
+        {
+            HangHoa hangHoa = db.HangHoas.Find(id);
+            db.HangHoas.Remove(hangHoa);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
---
+++ CuaHang_Nhom6/CuaHang/Controllers/LoaiHangController.cs
@@ -0,0 +1,74 @@
+﻿using CuaHang.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CuaHang.Controllers
+{
+    public class LoaiHangController : Controller
+    {
+        // GET: LoaiHang
+        CuaHangEntities1 db = new CuaHangEntities1();
+        public ActionResult Index(string txtSearch = "")
+        {
+            return View(db.LoaiHangs.Where(hh => hh.TenLH.Contains(txtSearch)));
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(LoaiHang LoaiHang)
+        {
+            if (ModelState.IsValid)
+            {
+                db.LoaiHangs.Add(LoaiHang);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(LoaiHang);
+        }
+        public ActionResult Details(int id)
+        {
+            return View(db.LoaiHangs.Find(id));
+        }
+        public ActionResult Edit(int id)
+        {
+            return View(db.LoaiHangs.Find(id));
+        }
+        [HttpPost]
+        public ActionResult Edit(LoaiHang LoaiHang)
+        {
+            if (ModelState.IsValid)
+            {
+                LoaiHang hh = db.LoaiHangs.Find(LoaiHang.MaLH);
+                hh.TenLH = LoaiHang.TenLH;
+
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(LoaiHang);
+        }
+        public ActionResult Delete(int id)
+        {
+            return View(db.LoaiHangs.Find(id));
+        }
+        public ActionResult ConfirmDelete(int id)
+        {
+            HangHoa hangHoa = db.HangHoas.FirstOrDefault(hh => hh.MaLH == id);
+            if (hangHoa == null)
+            {
+                LoaiHang LoaiHang = db.LoaiHangs.Find(id);
+                db.LoaiHangs.Remove(LoaiHang);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            TempData["ErrorMessage"] = "Có hàng hoá đang tham chiếu";
+            return RedirectToAction("Delete", "LoaiHang", new { id = id });
+        }
+    }
+}
